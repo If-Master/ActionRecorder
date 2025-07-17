127f68e55839fe98acfba7686b718123620aa7fe
If-Master <if.masters.kan@gmail.com>
2025-07-17 19:36:09 +0300

    Update ActionrecorderDataGenerator.java

--- src/client/java/me/kanuunankuulaspluginsMobs/actionrecorder/client/ActionrecorderDataGenerator.java
+++ src/client/java/me/kanuunankuulaspluginsMobs/actionrecorder/client/ActionrecorderDataGenerator.java
@@ -1,12 +1,1 @@
-//package me.kanuunankuulaspluginsMobs.actionrecorder.client;
-//
-//import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
-//import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-//
-//public class ActionrecorderDataGenerator implements DataGeneratorEntrypoint {
-//
-//    @Override
-//    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-//        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
-//    }
-//}
+// Ignore this one Forgot to delete it ngl
